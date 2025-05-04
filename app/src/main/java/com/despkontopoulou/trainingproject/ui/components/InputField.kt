5e12533a3d0794feef5bcb50bf9d12a347a3f033
despkontopoulou <despkontopoulou@gmail.com>
2025-05-04 22:23:21 +0300

    initial structure and color theme creation

---
+++ app/src/main/java/com/despkontopoulou/trainingproject/ui/components/InputField.kt
@@ -0,0 +1,2 @@
+package com.despkontopoulou.trainingproject.ui.components
+
---
+++ app/src/main/java/com/despkontopoulou/trainingproject/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.despkontopoulou.trainingproject.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xFFFFFFFF)
+val Red = Color(0xFFEC1C24)
+val LightRed = Color(0xFFFF4444)
+val Green = Color(0xFF28961B)
+val LightGreen = Color(0xFF86BD7F)
+val Black = Color(0xFF101417)
+val LightBlack=Color(0xFF1B1B1D)
+val Grey = Color(0xA6151617)
+val LightGrey = Color(0x801B1B1D)
+val LightBlue = Color(0xFF5FA2DD)
